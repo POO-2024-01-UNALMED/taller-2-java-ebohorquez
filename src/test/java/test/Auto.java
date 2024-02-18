d58ad79353c9f4237f0b3b32874cc9595295bb73
ebohorquez <159493345+ebohorquez@users.noreply.github.com>
2024-02-18 18:21:30 -0500

    Add files via upload

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,51 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	
+	
+	int cantidadAsientos()
+	{
+		int cuantosAsientos = 0;
+		for (int i = 0 ; i < asientos.length; i++)
+		{
+			if(asientos[i] != null)
+			{
+				cuantosAsientos++;
+			}
+		}
+		return cuantosAsientos;	
+	}
+	
+	
+	
+	
+	String verificarIntegridad()
+	{
+		if (this.registro == this.motor.registro)
+		{
+			for (int i = 0 ; i < asientos.length; i++)
+			{
+				if (asientos[i] != null)
+				{
+					if(asientos[i].registro != registro)
+					{
+						return "Las piezas no son originales";
+								
+					}
+				}
+			}
+			return "Auto original";
+			
+		}else {
+			return "Las piezas no son originales";
+		}
+	}
+    
+}
